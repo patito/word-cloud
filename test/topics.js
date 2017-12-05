4502b0c96412bc2af6eeb2d5fe2b7b2d1933d3b5
patito <benatto@gmail.com>
2017-12-05 15:10:14 +0000

    Adding some unittests

---
+++ test/topics.js
@@ -0,0 +1,38 @@
+var request = require('supertest'),
+    url = 'http://127.0.0.1:3001';
+
+describe('Testing API Endpoints', function() {
+
+    describe('GET /api/v1/topics', function() {
+        it('it should return all topics', function(done) {
+            request(url)
+                .get('/api/v1/topics')
+                .expect(200, done);
+        });
+    });
+
+    describe('GET /api/v1/fakes', function() {
+        it('it should return 404 to fakes endpoint', function(done) {
+            request(url)
+                .get('/api/v1/fake')
+                .expect(404, done);
+        });
+    });
+
+    describe('GET /api/v1/1751295897__DJ', function() {
+        it('it should return 200 valid ID', function(done) {
+            request(url)
+                .get('/api/v1/topics/1751295897__DJ')
+                .expect(200, done);
+        });
+    });
+
+    describe('GET /api/v1/topics/Invalid_ID', function() {
+        it('it should return 404 for wrong ID', function(done) {
+            request(url)
+                .get('/api/v1/topics/Invalid_ID')
+                .expect(404, done);
+        });
+    });
+
+});
